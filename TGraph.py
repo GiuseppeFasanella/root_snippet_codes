b7f27b843a675fef02a3169638fae21b9c200968
Giuseppe <I am Amerigo, you know my mail>
2015-04-13 19:30:04 +0200

    graphs, labels and margins

---
+++ TGraph.py
@@ -0,0 +1,8 @@
+    res_graph_2[regions].SetMarkerSize(1.2)
+    res_graph_2[regions].SetMarkerStyle(21)
+    res_graph_2[regions].SetLineColor(ROOT.kRed)
+    res_graph_2[regions].SetMarkerColor(ROOT.kRed)
+    res_graph_2[regions].GetXaxis().SetTitleSize(0.042)
+    res_graph_2[regions].GetXaxis().SetTitle('m_{ee} [GeV]')
+    res_graph_2[regions].GetYaxis().SetTitleSize(0.052)
+    res_graph_2[regions].GetYaxis().SetTitle('#sigma_{fit}')
---
+++ TLatex.py
@@ -0,0 +1,4 @@
+cms =ROOT.TLatex(0.12,0.95,"CMS Internal")
+cms.SetNDC()
+cms.SetTextSize(0.04)
+cms.Draw()
