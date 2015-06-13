bba3436a75aca9e1c9a6f74c99cd88ce3718be27
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-06-13 13:54:25 +0200

    Update plot.C

--- root_style/plot.C
+++ root_style/plot.C
@@ -1,5 +1,20 @@
+//Version \#1 = if it's a macro that you want to compile:
+// root -l ~/rootlogon.C
+//.L plot.C
+//plotter()
+
+#include <TCanvas.h>
+void plotter(){
+  TCanvas *c=new TCanvas();
+  c->Draw();
+}
+
+//Version \#2 = if it's a quick macro that you don't want to compile
+//root -l plot.C
 {
-  gROOT->ProcessLine(".L setTDRStyle.cc");
-  setTDRStyle();
+  gROOT->ProcessLine(".L ~/rootlogon.C")
+  rootlogon();
+  //gROOT->ProcessLine(".L setTDRStyle.cc");
+  //setTDRStyle();
   //Plotting
 }
