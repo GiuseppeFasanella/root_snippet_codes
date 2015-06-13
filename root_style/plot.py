0073f4686fd3617cd45c601e44425d4e3c61028c
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-06-13 13:56:52 +0200

    Update plot.py

--- root_style/plot.py
+++ root_style/plot.py
@@ -1,7 +1,7 @@
 import ROOT
-#you must open root and compile once and for all ".L setTDRStyle.cc++"
-#Now load the generated library
-ROOT.gSystem.Load("setTDRStyle_cc.so")
-ROOT.setTDRStyle()
-#Start plotting
+#you must open root and compile once and for all ".L rootlogon.C++"                                                                                                          
+#Now load the generated library                                                                                                                                                 
+ROOT.gSystem.Load("rootlogon_C.so")
+ROOT.rootlogon()
+#Start plotting 
 
