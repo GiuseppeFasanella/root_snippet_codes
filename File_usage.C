87185c4c9d2303796ece31fe5e12af37971ecaae
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-07-22 14:41:02 +0200

    Update File_usage.C

--- File_usage.C
+++ File_usage.C
@@ -4,3 +4,12 @@ f->cd();
 //histo->Write();
 f->Write();                                                                                                                                                
 f->Close();
+
+ If option = NEW or CREATE   create a new file and open it for writing,
+                             if the file already exists the file is
+                             not opened.
+           = RECREATE        create a new file, if the file already
+                             exists it will be overwritten.
+           = UPDATE          open an existing file for writing.
+                             if no file exists, it is created.
+           = READ            open an existing file for reading (default)
