f46b266ef34eefe4da80c316dc7785513d7b43ee
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-10-20 11:52:49 +0200

    Update TLatex.py

--- TLatex.py
+++ TLatex.py
@@ -1,4 +1,4 @@
 cms =ROOT.TLatex(0.12,0.95,"CMS Internal")
-cms.SetNDC()
+cms.SetNDC() # importante cosi' non usi le coordinate assolute, ma le coordinate relative, in percentuale della canvas
 cms.SetTextSize(0.04)
 cms.Draw()
