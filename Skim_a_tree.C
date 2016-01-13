9fbb8b5fa21f70890fbd7d50e19ceb60f56552ca
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2016-01-13 17:31:48 +0100

    Update Skim_a_tree.C

--- Skim_a_tree.C
+++ Skim_a_tree.C
@@ -1,8 +1,5 @@
-TTree* addBranch_class::AddBranch_R9prime(TChain* originalChain, TString treename, bool isMC){
+TTree* addBranch_class::AddBranch_R9Eleprime(TChain* originalChain, TString treename, bool isMC){
   //to have a branch with r9prime                                                                                                                                                
-
-  TTree* newtree = new TTree(treename, treename);
-
   //From the original tree take R9 and eta                                                                                                                                       
   Float_t         R9Ele[3];
   Float_t         etaEle[3];
@@ -13,12 +10,17 @@ TTree* addBranch_class::AddBranch_R9prime(TChain* originalChain, TString treenam
   originalChain->SetBranchAddress("etaEle", etaEle);
   originalChain->SetBranchAddress("R9Ele", R9Ele);
 
+  TTree* newtree = new TTree(treename, treename);
   Float_t R9Eleprime[3];
-  R9Eleprime[0]=1;
-  R9Eleprime[1]=1;
-  R9Eleprime[2]=-999;
   newtree->Branch("R9Eleprime", R9Eleprime, "R9Eleprime[3]/F");
-  newtree->Fill();
+
+  Long64_t nentries= originalChain->GetEntries();
+  for(Long64_t ientry = 0; ientry< nentries; ientry++){
+    originalChain->GetEntry(ientry);
+    R9Eleprime[0]=1;
+    R9Eleprime[1]=1;
+    R9Eleprime[2]=-999;
+    newtree->Fill();
   }
 
   originalChain->SetBranchStatus("*",1);
