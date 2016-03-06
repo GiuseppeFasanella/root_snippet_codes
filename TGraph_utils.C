02392f8c209afe0adbcc0d4bffefc18ec176865f
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2016-03-06 13:48:33 +0100

    Update TGraph_utils.C

--- TGraph_utils.C
+++ TGraph_utils.C
@@ -12,4 +12,15 @@ gr->Draw("AP*")//senza A non disegna l'asse e non vedi proprio niente
   Double_t ex[n] = {5.,10.,30.};
   gr = new TGraphErrors(n,x,y,ex,ey);
   
+//questo in python
+
+import ROOT
+import numpy as np
+
+#Il trucco con i TGraph e' usa le liste di python fino all'ultimo perche' sono molto flessibili,                                                                                
+#e all'ultimo momento le casti ad array perche' i TGraph non sanno gestire le list                                                                                              
+list_x=[1.,2.,3.,4.]
+list_y=[1.,2.,3.,4.]
+graph=ROOT.TGraph(len(list_x),np.asarray(list_x),np.asarray(list_y))
+graph.Draw("AP*") #Mi raccomando Draw("A....") perche' senza la A non disegna l'asse e non vedi niente 
   
