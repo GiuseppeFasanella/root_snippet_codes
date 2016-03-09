0c6d321ddfd582d5a27801a525d9d8f20d26e052
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2016-03-09 10:30:57 +0100

    Update plot.py

--- root_style/plot.py
+++ root_style/plot.py
@@ -1,6 +1,12 @@
 import ROOT
 #you must open root and compile once and for all ".L rootlogon.C++"                                                                                                          
-#Now load the generated library                                                                                                                                                 
+#Now load the generated library  
+#ROOT.gSystem.Load("rootlogon_C.so")
+
+#####Siccome root e' fesso, a volte non capisce niente tra nuova versione, vecchia versione....
+##Percio' compilalo al volo e poi fai il load del rootlogon
+import os
+os.system("root -l -b -q ~/compile_logon.C") #compile_logon e' definito piu' sotto
 ROOT.gSystem.Load("rootlogon_C.so")
 #puo' capitare a volte che il .so sia stato compilato con una versione differente di root
 #--> cancella rootlogon_C.so (per sicurezza) e ricompila con ++. 
@@ -8,3 +14,9 @@ ROOT.gSystem.Load("rootlogon_C.so")
 ROOT.rootlogon()
 #Start plotting 
 ROOT.gROOT.SetBatch(ROOT.kTRUE) #Do not show-up canvases
+
+
+##compile_logon.C e' fatto cosi'
+#{
+#  gROOT->ProcessLine(".L ~/rootlogon.C++");
+#}
